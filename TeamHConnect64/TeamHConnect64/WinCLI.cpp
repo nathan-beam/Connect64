79743eb80e605cd95a9b8f30f8410af4017320f0
Matt Maginniss <maginniss.matt@gmail.com>
2016-04-05 15:54:07 -0400

    Initial Commit

---
+++ TeamHConnect64/TeamHConnect64/WinCLI.cpp
@@ -0,0 +1,13 @@
+#include "MyForm.h"
+
+using namespace Connect64;
+
+[STAThreadAttribute]
+int main(array<System::String ^> ^args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Application::Run(gcnew MyForm());
+	return 0;
+}
