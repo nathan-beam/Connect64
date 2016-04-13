d29e1c832c075c7697b6019e1c127b351b7c0d7f
Nathan Beam <nbeam2@my.westga.edu>
2016-04-13 00:55:57 -0400

    View can now load in a prespecified table. Next to do is input

--- TeamHConnect64/TeamHConnect64/Connect64Form.h
+++ TeamHConnect64/TeamHConnect64/Connect64Form.h
@@ -1,4 +1,6 @@
 #pragma once
+#include "Board.h"
+using namespace model;
 
 namespace Connect64 {
 
@@ -29,75 +31,203 @@ namespace Connect64 {
 	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  gameToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  settingsToolStripMenuItem;
-	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel1;
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::Label^  label64;
-	private: System::Windows::Forms::Label^  label63;
-	private: System::Windows::Forms::Label^  label62;
-	private: System::Windows::Forms::Label^  label61;
-	private: System::Windows::Forms::Label^  label60;
-	private: System::Windows::Forms::Label^  label59;
-	private: System::Windows::Forms::Label^  label56;
-	private: System::Windows::Forms::Label^  label55;
-	private: System::Windows::Forms::Label^  label3;
-	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  label4;
-	private: System::Windows::Forms::Label^  label10;
-	private: System::Windows::Forms::Label^  label9;
-	private: System::Windows::Forms::Label^  label7;
-	private: System::Windows::Forms::Label^  label8;
-	private: System::Windows::Forms::Label^  label6;
-	private: System::Windows::Forms::Label^  label5;
-	private: System::Windows::Forms::Label^  label16;
-	private: System::Windows::Forms::Label^  label15;
-	private: System::Windows::Forms::Label^  label14;
-	private: System::Windows::Forms::Label^  label17;
-	private: System::Windows::Forms::Label^  label13;
-	private: System::Windows::Forms::Label^  label18;
-	private: System::Windows::Forms::Label^  label12;
-	private: System::Windows::Forms::Label^  label11;
-	private: System::Windows::Forms::Label^  label24;
-	private: System::Windows::Forms::Label^  label23;
-	private: System::Windows::Forms::Label^  label22;
-	private: System::Windows::Forms::Label^  label25;
-	private: System::Windows::Forms::Label^  label21;
-	private: System::Windows::Forms::Label^  label26;
-	private: System::Windows::Forms::Label^  label20;
-	private: System::Windows::Forms::Label^  label19;
-	private: System::Windows::Forms::Label^  label30;
-	private: System::Windows::Forms::Label^  label31;
-	private: System::Windows::Forms::Label^  label29;
-	private: System::Windows::Forms::Label^  label32;
-	private: System::Windows::Forms::Label^  label28;
-	private: System::Windows::Forms::Label^  label27;
-	private: System::Windows::Forms::Label^  label38;
-	private: System::Windows::Forms::Label^  label37;
-	private: System::Windows::Forms::Label^  label36;
-	private: System::Windows::Forms::Label^  label39;
-	private: System::Windows::Forms::Label^  label35;
-	private: System::Windows::Forms::Label^  label40;
-	private: System::Windows::Forms::Label^  label34;
-	private: System::Windows::Forms::Label^  label33;
-	private: System::Windows::Forms::Label^  label46;
-	private: System::Windows::Forms::Label^  label45;
-	private: System::Windows::Forms::Label^  label44;
-	private: System::Windows::Forms::Label^  label47;
-	private: System::Windows::Forms::Label^  label43;
-	private: System::Windows::Forms::Label^  label48;
-	private: System::Windows::Forms::Label^  label42;
-	private: System::Windows::Forms::Label^  label41;
-	private: System::Windows::Forms::Label^  label52;
-	private: System::Windows::Forms::Label^  label53;
-	private: System::Windows::Forms::Label^  label51;
-	private: System::Windows::Forms::Label^  label54;
-	private: System::Windows::Forms::Label^  label50;
-	private: System::Windows::Forms::Label^  label49;
-	private: System::Windows::Forms::Label^  label58;
-	private: System::Windows::Forms::Label^  label57;
+	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel;
+			 Board^ gameBoard;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	private: System::Windows::Forms::ToolStripMenuItem^  saveToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  loadToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  resetToolStripMenuItem;
 	private: System::Windows::Forms::ToolStripMenuItem^  choosePuzzleToolStripMenuItem;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	private: System::Windows::Forms::Label^  label1;
+private: System::Windows::Forms::Label^  label8;
+private: System::Windows::Forms::Label^  label7;
+private: System::Windows::Forms::Label^  label6;
+private: System::Windows::Forms::Label^  label5;
+private: System::Windows::Forms::Label^  label4;
+private: System::Windows::Forms::Label^  label3;
+private: System::Windows::Forms::Label^  label2;
+private: System::Windows::Forms::Label^  label24;
+private: System::Windows::Forms::Label^  label23;
+private: System::Windows::Forms::Label^  label22;
+private: System::Windows::Forms::Label^  label21;
+private: System::Windows::Forms::Label^  label20;
+private: System::Windows::Forms::Label^  label19;
+private: System::Windows::Forms::Label^  label18;
+private: System::Windows::Forms::Label^  label17;
+private: System::Windows::Forms::Label^  label16;
+private: System::Windows::Forms::Label^  label15;
+private: System::Windows::Forms::Label^  label14;
+private: System::Windows::Forms::Label^  label13;
+private: System::Windows::Forms::Label^  label12;
+private: System::Windows::Forms::Label^  label11;
+private: System::Windows::Forms::Label^  label10;
+private: System::Windows::Forms::Label^  label9;
+private: System::Windows::Forms::Label^  label64;
+private: System::Windows::Forms::Label^  label63;
+private: System::Windows::Forms::Label^  label62;
+private: System::Windows::Forms::Label^  label61;
+private: System::Windows::Forms::Label^  label60;
+private: System::Windows::Forms::Label^  label59;
+private: System::Windows::Forms::Label^  label58;
+private: System::Windows::Forms::Label^  label57;
+private: System::Windows::Forms::Label^  label56;
+private: System::Windows::Forms::Label^  label55;
+private: System::Windows::Forms::Label^  label54;
+private: System::Windows::Forms::Label^  label53;
+private: System::Windows::Forms::Label^  label52;
+private: System::Windows::Forms::Label^  label51;
+private: System::Windows::Forms::Label^  label50;
+private: System::Windows::Forms::Label^  label49;
+private: System::Windows::Forms::Label^  label48;
+private: System::Windows::Forms::Label^  label47;
+private: System::Windows::Forms::Label^  label46;
+private: System::Windows::Forms::Label^  label45;
+private: System::Windows::Forms::Label^  label44;
+private: System::Windows::Forms::Label^  label43;
+private: System::Windows::Forms::Label^  label42;
+private: System::Windows::Forms::Label^  label41;
+private: System::Windows::Forms::Label^  label40;
+private: System::Windows::Forms::Label^  label39;
+private: System::Windows::Forms::Label^  label38;
+private: System::Windows::Forms::Label^  label37;
+private: System::Windows::Forms::Label^  label36;
+private: System::Windows::Forms::Label^  label35;
+private: System::Windows::Forms::Label^  label34;
+private: System::Windows::Forms::Label^  label33;
+private: System::Windows::Forms::Label^  label32;
+private: System::Windows::Forms::Label^  label31;
+private: System::Windows::Forms::Label^  label30;
+private: System::Windows::Forms::Label^  label29;
+private: System::Windows::Forms::Label^  label28;
+private: System::Windows::Forms::Label^  label27;
+private: System::Windows::Forms::Label^  label26;
+private: System::Windows::Forms::Label^  label25;
 
 
 
@@ -108,6 +238,8 @@ namespace Connect64 {
 		/// </summary>
 		System::ComponentModel::Container ^components;
 
+		void setBoard();
+		void showBoard();
 
 #pragma region Windows Form Designer generated code
 		/// <summary>
@@ -116,6 +248,9 @@ namespace Connect64 {
 		/// </summary>
 		void InitializeComponent(void);
 
+private: System::Void tableLayoutPanel_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e);
+
+private: System::Void label_Click(System::Object^  sender, System::EventArgs^  e);
 };
 }
 
--- TeamHConnect64/TeamHConnect64/Controller/Connect64Form.cpp
+++ TeamHConnect64/TeamHConnect64/Controller/Connect64Form.cpp
@@ -14,7 +14,8 @@ namespace Connect64 {
 		this->loadToolStripMenuItem->Text = this->resourceManager->GetString("LoadMenuItemText");
 		this->resetToolStripMenuItem->Text = this->resourceManager->GetString("ResetMenuItemText");
 		this->choosePuzzleToolStripMenuItem->Text = this->resourceManager->GetString("ChoosePuzzleMenuItemText");
-
+		this->gameBoard = gcnew Board();
+		this->setBoard();
 	}
 
 	/// <summary>
@@ -27,5 +28,54 @@ namespace Connect64 {
 			delete components;
 		}
 	}
+	//Placeholder for now - Just going for functionality
+	void Connect64Form::setBoard(){
+		this->gameBoard->setTile(0, 0, 1);
+		this->gameBoard->setTile(7, 0, 8);
+		this->gameBoard->setTile(7, 7, 15);
+		this->gameBoard->setTile(0, 7, 22);
+		this->gameBoard->setTile(1, 1, 29);
+		this->gameBoard->setTile(6, 1, 44);
+		this->gameBoard->setTile(6, 6, 39);
+		this->gameBoard->setTile(1, 6, 34);
+		this->gameBoard->setTile(2, 3, 56);
+		this->gameBoard->setTile(5, 3, 49);
+		this->gameBoard->setTile(5, 5, 61);
+		this->gameBoard->setTile(2, 5, 58);
+		this->showBoard();
+	}
 
+	void Connect64Form::showBoard(){
+		for each (Object^ control in this->tableLayoutPanel->Controls){
+			Label^ label = safe_cast<Label^>(control);
+			int x = this->tableLayoutPanel->GetColumn(label);
+			int y = this->tableLayoutPanel->GetRow(label);
+			int num = this->gameBoard->getTile(x, y);
+			if (num != 0){
+				label->Text = num + "";
+			}
+		}
+	}
+	void Connect64Form::tableLayoutPanel_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e){
+		TextBox^ tempEntryBox = gcnew TextBox();
+		Point^ coords = gcnew Point(e->X, e->Y);
+		Object^ control = this->tableLayoutPanel->GetChildAtPoint(*coords);
+		Label^ label = safe_cast<Label^>(control);
+		label->Text = "C";
+		int x = this->tableLayoutPanel->GetColumn(label);
+		int y = this->tableLayoutPanel->GetRow(label);
+		this->tableLayoutPanel->SetColumn(tempEntryBox, x);
+		this->tableLayoutPanel->SetRow(tempEntryBox, y);
+
+	}
+
+	void Connect64Form::label_Click(System::Object^  sender, System::EventArgs^  e){
+		TextBox^ tempEntryBox = gcnew TextBox();
+		Label^ label = safe_cast<Label^>(sender);
+		label->Text = "C";
+		int x = this->tableLayoutPanel->GetColumn(label);
+		int y = this->tableLayoutPanel->GetRow(label);
+		this->tableLayoutPanel->SetColumn(tempEntryBox, x);
+		this->tableLayoutPanel->SetRow(tempEntryBox, y);
+	}
 }
