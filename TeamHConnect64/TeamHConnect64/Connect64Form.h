10c55c53722fb00ef6a14e4fb462ca2ac4f992de
Nathan Beam <nbeam2@my.westga.edu>
2016-04-14 14:32:48 -0400

    Dont know what this is but heres a commit

--- TeamHConnect64/TeamHConnect64/Connect64Form.h
+++ TeamHConnect64/TeamHConnect64/Connect64Form.h
@@ -28,96 +28,96 @@ namespace Connect64 {
 		protected:
 		~Connect64Form();
 
-		private: ResourceManager^ resourceManager;
+		 ResourceManager^ resourceManager;
 
-
-	private: System::Windows::Forms::MenuStrip^  menuStrip1;
-	private: System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  gameToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  settingsToolStripMenuItem;
-	private: System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel;
+	private:
+	 System::Windows::Forms::MenuStrip^  menuStrip1;
+	 System::Windows::Forms::ToolStripMenuItem^  fileToolStripMenuItem;
+	 System::Windows::Forms::ToolStripMenuItem^  gameToolStripMenuItem;
+	 System::Windows::Forms::ToolStripMenuItem^  settingsToolStripMenuItem;
+	 System::Windows::Forms::TableLayoutPanel^  tableLayoutPanel;
 			 Board^ gameBoard;
 			 ConnectFileIO^ fileIO;
 
-	private: System::Windows::Forms::ToolStripMenuItem^  saveToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  loadToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  resetToolStripMenuItem;
-	private: System::Windows::Forms::ToolStripMenuItem^  choosePuzzleToolStripMenuItem;
+	 System::Windows::Forms::ToolStripMenuItem^  saveToolStripMenuItem;
+	 System::Windows::Forms::ToolStripMenuItem^  loadToolStripMenuItem;
+	 System::Windows::Forms::ToolStripMenuItem^  resetToolStripMenuItem;
+	 System::Windows::Forms::ToolStripMenuItem^  choosePuzzleToolStripMenuItem;
 
 
-	private: System::Windows::Forms::Label^  label1;
-	private: System::Windows::Forms::Label^  label8;
-	private: System::Windows::Forms::Label^  label7;
-	private: System::Windows::Forms::Label^  label6;
-	private: System::Windows::Forms::Label^  label5;
-	private: System::Windows::Forms::Label^  label4;
-	private: System::Windows::Forms::Label^  label3;
-	private: System::Windows::Forms::Label^  label2;
-	private: System::Windows::Forms::Label^  label24;
-	private: System::Windows::Forms::Label^  label23;
-	private: System::Windows::Forms::Label^  label22;
-	private: System::Windows::Forms::Label^  label21;
-	private: System::Windows::Forms::Label^  label20;
-	private: System::Windows::Forms::Label^  label19;
-	private: System::Windows::Forms::Label^  label18;
-	private: System::Windows::Forms::Label^  label17;
-	private: System::Windows::Forms::Label^  label16;
-	private: System::Windows::Forms::Label^  label15;
-	private: System::Windows::Forms::Label^  label14;
-	private: System::Windows::Forms::Label^  label13;
-	private: System::Windows::Forms::Label^  label12;
-	private: System::Windows::Forms::Label^  label11;
-	private: System::Windows::Forms::Label^  label10;
-	private: System::Windows::Forms::Label^  label9;
-	private: System::Windows::Forms::Label^  label64;
-	private: System::Windows::Forms::Label^  label63;
-	private: System::Windows::Forms::Label^  label62;
-	private: System::Windows::Forms::Label^  label61;
-	private: System::Windows::Forms::Label^  label60;
-	private: System::Windows::Forms::Label^  label59;
-	private: System::Windows::Forms::Label^  label58;
-	private: System::Windows::Forms::Label^  label57;
-	private: System::Windows::Forms::Label^  label56;
-	private: System::Windows::Forms::Label^  label55;
-	private: System::Windows::Forms::Label^  label54;
-	private: System::Windows::Forms::Label^  label53;
-	private: System::Windows::Forms::Label^  label52;
-	private: System::Windows::Forms::Label^  label51;
-	private: System::Windows::Forms::Label^  label50;
-	private: System::Windows::Forms::Label^  label49;
-	private: System::Windows::Forms::Label^  label48;
-	private: System::Windows::Forms::Label^  label47;
-	private: System::Windows::Forms::Label^  label46;
-	private: System::Windows::Forms::Label^  label45;
-	private: System::Windows::Forms::Label^  label44;
-	private: System::Windows::Forms::Label^  label43;
-	private: System::Windows::Forms::Label^  label42;
-	private: System::Windows::Forms::Label^  label41;
-	private: System::Windows::Forms::Label^  label40;
-	private: System::Windows::Forms::Label^  label39;
-	private: System::Windows::Forms::Label^  label38;
-	private: System::Windows::Forms::Label^  label37;
-	private: System::Windows::Forms::Label^  label36;
-	private: System::Windows::Forms::Label^  label35;
-	private: System::Windows::Forms::Label^  label34;
-	private: System::Windows::Forms::Label^  label33;
-	private: System::Windows::Forms::Label^  label32;
-	private: System::Windows::Forms::Label^  label31;
-	private: System::Windows::Forms::Label^  label30;
-	private: System::Windows::Forms::Label^  label29;
-	private: System::Windows::Forms::Label^  label28;
-	private: System::Windows::Forms::Label^  label27;
-	private: System::Windows::Forms::Label^  label26;
-	private: System::Windows::Forms::Label^  label25;
-	private: System::Windows::Forms::NumericUpDown^  numericUpDown;
-	private: System::Windows::Forms::Button^  confirmInputButton;
+	 System::Windows::Forms::Label^  label1;
+	 System::Windows::Forms::Label^  label8;
+	 System::Windows::Forms::Label^  label7;
+	 System::Windows::Forms::Label^  label6;
+	 System::Windows::Forms::Label^  label5;
+	 System::Windows::Forms::Label^  label4;
+	 System::Windows::Forms::Label^  label3;
+	 System::Windows::Forms::Label^  label2;
+	 System::Windows::Forms::Label^  label24;
+	 System::Windows::Forms::Label^  label23;
+	 System::Windows::Forms::Label^  label22;
+	 System::Windows::Forms::Label^  label21;
+	 System::Windows::Forms::Label^  label20;
+	 System::Windows::Forms::Label^  label19;
+	 System::Windows::Forms::Label^  label18;
+	 System::Windows::Forms::Label^  label17;
+	 System::Windows::Forms::Label^  label16;
+	 System::Windows::Forms::Label^  label15;
+	 System::Windows::Forms::Label^  label14;
+	 System::Windows::Forms::Label^  label13;
+	 System::Windows::Forms::Label^  label12;
+	 System::Windows::Forms::Label^  label11;
+	 System::Windows::Forms::Label^  label10;
+	 System::Windows::Forms::Label^  label9;
+	 System::Windows::Forms::Label^  label64;
+	 System::Windows::Forms::Label^  label63;
+	 System::Windows::Forms::Label^  label62;
+	 System::Windows::Forms::Label^  label61;
+	 System::Windows::Forms::Label^  label60;
+	 System::Windows::Forms::Label^  label59;
+	 System::Windows::Forms::Label^  label58;
+	 System::Windows::Forms::Label^  label57;
+	 System::Windows::Forms::Label^  label56;
+	 System::Windows::Forms::Label^  label55;
+	 System::Windows::Forms::Label^  label54;
+	 System::Windows::Forms::Label^  label53;
+	 System::Windows::Forms::Label^  label52;
+	 System::Windows::Forms::Label^  label51;
+	 System::Windows::Forms::Label^  label50;
+	 System::Windows::Forms::Label^  label49;
+	 System::Windows::Forms::Label^  label48;
+	 System::Windows::Forms::Label^  label47;
+	 System::Windows::Forms::Label^  label46;
+	 System::Windows::Forms::Label^  label45;
+	 System::Windows::Forms::Label^  label44;
+	 System::Windows::Forms::Label^  label43;
+	 System::Windows::Forms::Label^  label42;
+	 System::Windows::Forms::Label^  label41;
+	 System::Windows::Forms::Label^  label40;
+	 System::Windows::Forms::Label^  label39;
+	 System::Windows::Forms::Label^  label38;
+	 System::Windows::Forms::Label^  label37;
+	 System::Windows::Forms::Label^  label36;
+	 System::Windows::Forms::Label^  label35;
+	 System::Windows::Forms::Label^  label34;
+	 System::Windows::Forms::Label^  label33;
+	 System::Windows::Forms::Label^  label32;
+	 System::Windows::Forms::Label^  label31;
+	 System::Windows::Forms::Label^  label30;
+	 System::Windows::Forms::Label^  label29;
+	 System::Windows::Forms::Label^  label28;
+	 System::Windows::Forms::Label^  label27;
+	 System::Windows::Forms::Label^  label26;
+	 System::Windows::Forms::Label^  label25;
+	 System::Windows::Forms::NumericUpDown^  numericUpDown;
+	 System::Windows::Forms::Button^  confirmInputButton;
 
 		 Label^ editLabel;
 		 List<Label^>^ startingBoard;
-	private: System::Windows::Forms::Label^  puzzleNumberLabel;
+	 System::Windows::Forms::Label^  puzzleNumberLabel;
 
 
-		private:
+		
 		/// <summary>
 		/// Required designer variable.
 		/// </summary>
@@ -133,15 +133,15 @@ namespace Connect64 {
 		/// </summary>
 		void InitializeComponent(void);
 
-private: System::Void tableLayoutPanel_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e);
+ System::Void tableLayoutPanel_MouseDown(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e);
 
-private: System::Void label_Click(System::Object^  sender, System::EventArgs^  e);
-private: System::Void confirmInputButton_Click(System::Object^  sender, System::EventArgs^  e);
+ System::Void label_Click(System::Object^  sender, System::EventArgs^  e);
+ System::Void confirmInputButton_Click(System::Object^  sender, System::EventArgs^  e);
 		 void checkForDuplicates();
 		 void increaseUpDown();
-private: System::Void resetToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
-private: System::Void numericUpDown_KeyDown(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e);
-private: System::Void Connect64Form::choosePuzzleToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
+ System::Void resetToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
+ System::Void numericUpDown_KeyDown(System::Object^  sender, System::Windows::Forms::KeyEventArgs^  e);
+ System::Void Connect64Form::choosePuzzleToolStripMenuItem_Click(System::Object^  sender, System::EventArgs^  e);
 };
 }
 
